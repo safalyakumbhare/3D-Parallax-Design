f142152ce559ae54380ce8a023196afe37052285
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-05-19 23:51:14 +0530

    startup animation of text 1 done

--- js/app.js
+++ js/app.js
@@ -5,12 +5,7 @@ let xValue = 0,
 
 let rotateDegree = 0;
 
-window.addEventListener("mousemove", (e) => {
-  xValue = e.clientX - window.innerWidth / 2;
-  yValue = e.clientY - window.innerHeight / 2;
-  rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
-  console.log(rotateDegree);
-  console.log(xValue, yValue);
+function update(cursorPosition) {
   parallax_el.forEach((el) => {
     let speedx = el.dataset.speedx;
     let speedy = el.dataset.speedy;
@@ -22,7 +17,7 @@ window.addEventListener("mousemove", (e) => {
     let isInLeft =
       parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
     let zValue =
-      (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+      (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
 
     el.style.transform = `translateX(calc(-50% - ${
       -xValue * speedx
@@ -32,4 +27,42 @@ window.addEventListener("mousemove", (e) => {
       rotateDegree * rotateSpeed
     }deg)`;
   });
+}
+update(0);
+window.addEventListener("mousemove", (e) => {
+  xValue = e.clientX - window.innerWidth / 2;
+  yValue = e.clientY - window.innerHeight / 2;
+  rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+  console.log(rotateDegree);
+  console.log(xValue, yValue);
+
+  update(e.clientX);
 });
+
+// GASP Animation
+let timeline = gsap.timeline();
+
+Array.from(parallax_el)
+  .filter((el) => !el.classList.contains("text"))
+  .forEach((el) => {
+    timeline.from(
+      el,
+      {
+        top: `${el.offsetHeight / 2 + +el.dataset.distance}px`,
+        duration: 3.5,
+        ease: "power3.out",
+      },
+      "1"
+    );
+  });
+
+timeline.from(
+  ".text h1",
+  {
+    y:
+      window.innerHeight -
+      document.querySelector(".text h1").getBoundingClientRect().top+200,
+    duration: 2,
+  },
+  "2.5"
+);
